8c63800ccff0428a3b48443dbc91b8eefccb5c9a
Liam Grossman <liam.j.grossman@gmail.com>
2024-03-08 14:22:30 -0500

    Add a test

---
+++ src/test/java/CourseTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseTest {
+
+    @Test
+    void getCode() {
+        assertEquals(7,7);
+    }
+}
