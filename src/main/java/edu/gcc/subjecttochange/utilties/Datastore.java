4f767eecc042a2ef46f9ab86b21aa46e7eebcd2c
Liam Grossman <liam.j.grossman@gmail.com>
2024-03-11 09:49:08 -0400

    Create a datastore to persist data

---
+++ src/main/java/edu/gcc/subjecttochange/utilties/Datastore.java
@@ -0,0 +1,19 @@
+package edu.gcc.subjecttochange.utilties;
+
+import edu.gcc.subjecttochange.models.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+
+public class Datastore {
+    public static final ArrayList<Student> students = new ArrayList<>();
+
+    public static final ArrayList<Course> courses = new ArrayList<>();
+    public static final HashMap<Search, List<Course>> searchHistory = new HashMap<>();
+
+    public static Optional<Student> getStudent(int studentId) {
+        return students.stream().filter(item -> item.id == studentId).findFirst();
+    }
+}
