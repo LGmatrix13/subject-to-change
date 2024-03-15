9289306df06a48f7df8bad676b7516817c10039a
Liam Grossman <liam.j.grossman@gmail.com>
2024-03-14 20:03:56 -0400

    Add endpoint for generating suggested schedule

--- src/main/java/edu/gcc/subjecttochange/controllers/SearchController.java
+++ src/main/java/edu/gcc/subjecttochange/controllers/SearchController.java
@@ -1,7 +1,13 @@
 package edu.gcc.subjecttochange.controllers;
 
+import edu.gcc.subjecttochange.models.Course;
 import edu.gcc.subjecttochange.models.Search;
+import edu.gcc.subjecttochange.models.Student;
+import edu.gcc.subjecttochange.utilties.Datastore;
 import io.javalin.http.Context;
+
+import java.util.List;
+import java.util.Optional;
 
 
 public class SearchController {
@@ -14,5 +20,29 @@ public class SearchController {
         );
         context.json(search.run());
         context.status(200);
+    }
+
+    public static void getGenerateSchedule(Context context) {
+        String studentId = Student.getStudentId(context);
+        Optional<Student> student = Datastore.getStudent(studentId);
+
+        if (student.isPresent()) {
+            String major = student.get().major;
+            Search search = new Search(
+                major,
+                null,
+                null,
+                null
+            );
+            List<Course> candidateCourses = search.run().stream().filter(
+                course -> !student.get().fallSchedule.contains(course) || !student.get().springSchedule.contains(course)
+            ).distinct().limit(7).toList();
+            context.json(candidateCourses);
+            context.status(200);
+            return;
+        }
+
+        context.result("Could not generate student schedule");
+        context.status(400);
     }
 }
