2f376c540a063da2a1e4895446434a584793ea0f
elliot-eyre <134098343+elliot-eyre@users.noreply.github.com>
2024-03-17 20:44:51 -0400

    Handled Scheduling Conflicts/Issues

--- src/main/java/edu/gcc/subjecttochange/models/Course.java
+++ src/main/java/edu/gcc/subjecttochange/models/Course.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 
@@ -43,4 +45,31 @@ public class Course {
     public Professor professor;
     @JsonProperty("waitlist")
     public List<Student> waitlist;
+
+    public boolean isFull() {
+        return enrolled >= seats;
+    }
+
+    public boolean conflictsWith(Course otherCourse) {
+        // Convert start and end times to minutes for easier comparison
+        int thisStartMinutes = timeToMinutes(this.startTime);
+        int thisEndMinutes = timeToMinutes(this.endTime);
+        int otherStartMinutes = timeToMinutes(otherCourse.startTime);
+        int otherEndMinutes = timeToMinutes(otherCourse.endTime);
+
+        // Check for overlap
+        return !(thisEndMinutes <= otherStartMinutes || thisStartMinutes >= otherEndMinutes);
+    }
+
+    private int timeToMinutes(String timeString) {
+        // Convert time string to minutes
+        String[] parts = timeString.split(":");
+        int hours = Integer.parseInt(parts[0]);
+        int minutes = Integer.parseInt(parts[1].split(" ")[0]);
+        String amPm = parts[1].split(" ")[1];
+        if (amPm.equals("PM")) {
+            hours += 12;
+        }
+        return hours * 60 + minutes;
+    }
 }
