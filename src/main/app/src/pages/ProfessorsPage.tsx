467c145e607b4af6bfe50a7e67915383bf877c05
elliot-eyre <134098343+elliot-eyre@users.noreply.github.com>
2024-03-18 22:23:56 -0400

    Adjusted Professors UI and connected it to professors.json

--- src/main/app/src/pages/ProfessorsPage.tsx
+++ src/main/app/src/pages/ProfessorsPage.tsx
@@ -1,64 +1,33 @@
 import React from "react";
 import ProfessorCard from "../components/ProfessorCard";
-// import { useSelector } from "react-redux";
+import useSWR from "swr";
+import { fetcher } from "../utils/fetcher";
 
 export default function ProfessorsPage() {
-//   const fallSchedule = useSelector((state) => state.student.fallSchedule);
-//   const springSchedule = useSelector((state) => state.student.springSchedule);
-//
-//   const allCourses = [...fallSchedule.courses, ...springSchedule.courses];
-//
-//   const professorCards = allCourses.map((course, index) => (
-//     <ProfessorCard
-//       key={index}
-//       firstName={course.professor.firstName}
-//       lastName={course.professor.lastName}
-//       department={course.professor.department}
-//     />
-//   ));
-    const [user] = useLocalStorage("user", {
-        id: "",
-      });
+  const { data: professors, error } = useSWR("/professors.json", fetcher);
 
-      const { data, isLoading } = useSWR<Professor[]>(
-        "http://localhost:7070/api/professors",
-        (url: string) => fetcher(url, user.id)
-      );
+  if (error) return <div>Error loading data</div>;
+  if (!professors) return <div>Loading...</div>;
+
+  const professorCards = professors.map((professor, index) => (
+    <ProfessorCard
+      key={index}
+      firstName={professor.firstName}
+      lastName={professor.lastName}
+      department={professor.department}
+      roomNumber={professor.roomNumber}
+      officeHours={professor.officeHours}
+      bio={professor.bio}
+    />
+  ));
 
   return (
-//     <div className="grid grid-cols-3 gap-5">
-//       {professorCards.length > 0 ? (
-//         professorCards
-//       ) : (
-//         <p>Schedule Courses to View Your Professors!</p>
-//       )}
-//     </div>
     <div className="grid grid-cols-3 gap-5">
-          <ProfessorCard
-            firstName="Test"
-            lastName="Test"
-            department="Computer Science"
-          />
-          <ProfessorCard
-            firstName="Test"
-            lastName="Test"
-            department="Computer Science"
-          />
-          <ProfessorCard
-            firstName="Test"
-            lastName="Test"
-            department="Computer Science"
-          />
-          <ProfessorCard
-            firstName="Test"
-            lastName="Test"
-            department="Computer Science "
-          />
-          <ProfessorCard
-            firstName="Test"
-            lastName="Test"
-            department="Computer Science"
-          />
-        </div>
+      {professorCards.length > 0 ? (
+        professorCards
+      ) : (
+        <p>No professors available</p>
+      )}
+    </div>
   );
 }
