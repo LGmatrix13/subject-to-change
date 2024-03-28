bdc13d8a91ee0799c0ca275def86414ce693650e
Liam Grossman <liam.j.grossman@gmail.com>
2024-03-27 20:29:36 -0400

    Add new build of UI

--- src/main/app/src/layouts/SearchLayout.tsx
+++ src/main/app/src/layouts/SearchLayout.tsx
@@ -1,9 +1,5 @@
 import React from "react";
-import useLocalStorage from "../hooks/useLocalStorage";
-import { FALL, SPRING } from "../utils/constants";
-import containerBackground from "/containerBackground.png";
 import BaseLayout from "./BaseLayout";
-import { useSearchParams } from "react-router-dom";
 
 interface SearchLayoutProps {
   children: React.ReactElement;
