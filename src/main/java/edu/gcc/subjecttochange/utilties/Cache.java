fb56e5e30f5e7f2bfabb7e0bededdda07cc22537
Liam Grossman <liam.j.grossman@gmail.com>
2024-04-13 13:23:18 -0400

    Update Cache.java

--- src/main/java/edu/gcc/subjecttochange/utilties/Cache.java
+++ src/main/java/edu/gcc/subjecttochange/utilties/Cache.java
@@ -2,10 +2,8 @@ package edu.gcc.subjecttochange.utilties;
 
 import edu.gcc.subjecttochange.models.*;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
 
 public class Cache {
     public static final HashMap<Search, List<Course>> searchHistory = new HashMap<>();
