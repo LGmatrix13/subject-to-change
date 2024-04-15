351aeec46055e63e73d254f59c4c040025a88979
Liam Grossman <liam.j.grossman@gmail.com>
2024-04-15 15:49:17 -0400

    Put auth logic in main. Update ui build.

--- src/main/java/edu/gcc/subjecttochange/controllers/SuggestedController.java
+++ src/main/java/edu/gcc/subjecttochange/controllers/SuggestedController.java
@@ -21,28 +21,22 @@ public class SuggestedController {
         Integer studentId = JWT.decodeStudentId(context);
         String semester = context.req().getParameter("semester");
         // if student exists, proceed
-        if (studentId != null) {
-            List<CourseDto> courseDtos = Database.query("""
-                select c."department", c."number", c."semester", c."hours", 
-                c."name", c."startTime", c."endTime", c."weekday", c."section", c."seats", 
-                c."enrolled", p."firstName" "professorFirstName", p."lastName" "professorLastName"
-                from course c
-                join professor p on c."professorId" = p."id"
-                where c."department" = (
-                    select "major" from "student"
-                    where "id" = ?                    
-                ) and c."name" != (
-                    select "name" from "course" c
-                    join "schedule" s on s."courseId" = c."id" 
-                    where s."studentId" = ?
-                ) and c."semester" = ?;  
-                
-            """, CourseDto.class, studentId, studentId, semester);
-            Response.send(200, context, courseDtos);
-            return;
-        }
-
-        // otherwise notify the student a schedule could not be generated
-        Response.send(400, context, "Could not generate student schedule");
+        List<CourseDto> courseDtos = Database.query("""
+            select c."department", c."number", c."semester", c."hours", 
+            c."name", c."startTime", c."endTime", c."weekday", c."section", c."seats", 
+            c."enrolled", p."firstName" "professorFirstName", p."lastName" "professorLastName"
+            from course c
+            join professor p on c."professorId" = p."id"
+            where c."department" = (
+                select "major" from "student"
+                where "id" = ?                    
+            ) and c."name" != (
+                select "name" from "course" c
+                join "schedule" s on s."courseId" = c."id" 
+                where s."studentId" = ?
+            ) and c."semester" = ?;  
+            
+        """, CourseDto.class, studentId, studentId, semester);
+        Response.send(200, context, courseDtos);
     }
 }
