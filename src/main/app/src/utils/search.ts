e36ffcb0d31a43631fbec1874f760624abc5745c
jminnich23 <minnichjs21@gcc.edu>
2024-05-01 12:44:28 -0400

    Created Search.ts

--- src/main/app/src/utils/search.ts
+++ src/main/app/src/utils/search.ts
@@ -1,8 +1,72 @@
-import type { Course } from "./types";
+import { Course } from "./types";
 
 class Search {
-  static byName(): Course[] {}
-  static byProfessor(): Course[] {}
-  static byNumber(): Course[] {}
-  // add the rest
+    static byName(courses: Course[], name: string) {
+        const filteredCourses = []
+        for (const course of courses) {
+            if (course.name.toLowerCase() === name.toLowerCase()) {
+                filteredCourses.push(course)
+            }
+        }
+        return filteredCourses;
+    }
+    static byDepartment(courses: Course[], department: string) {
+        const filteredCourses = []
+        for (const course of courses) {
+            if (course.department === department) {
+                filteredCourses.push(course)
+            }
+        }
+        return filteredCourses;
+    } 
+    static byNumber(courses: Course[], number: number) {
+        const filteredCourses = []
+        for (const course of courses) {
+            if (course.number == number) {
+                filteredCourses.push(course)
+            }
+        }
+        return filteredCourses;
+    } 
+    static byProffesor(courses: Course[], professorFN: string, proffesorLN: string) {
+        const filteredCourses = []
+        for (const course of courses) {
+            if (course.professorFirstName === professorFN && course.professorLastName === proffesorLN) {
+                filteredCourses.push(course)
+            }
+        }
+        return filteredCourses;
+    } 
+    static byWeekday(courses: Course[], weekday: string) {
+        const filteredCourses = []
+        for (const course of courses) {
+            if (course.weekday === weekday) {
+                filteredCourses.push(course)
+            }
+        }
+        return filteredCourses;
+    } 
+    static byStartTime(courses: Course[], startTime: string) {
+        const filteredCourses = []
+        const startTimeDate = new Date(startTime);
+        for (const course of courses) {
+            const courseStartTimeDate = new Date(course.startTime || "")
+            if (courseStartTimeDate >= startTimeDate) {
+                filteredCourses.push(course)
+            }
+        }
+        return filteredCourses;
+    } 
+    static byEndTime(courses: Course[], endTime: string) {
+        const filteredCourses = []
+        const endTimeDate = new Date(endTime);
+        for (const course of courses) {
+            const courseEndTimeDate = new Date(course.endTime || "")
+            if (courseEndTimeDate <= endTimeDate) {
+                filteredCourses.push(course)
+            }
+        }
+        return filteredCourses;
+    } 
+    
 }
