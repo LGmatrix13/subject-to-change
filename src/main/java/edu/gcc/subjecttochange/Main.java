a21174c72a7712757266bc90e3548c0974fb7aa1
Liam Grossman <liam.j.grossman@gmail.com>
2024-05-01 12:02:10 -0400

    fix invalid date error

--- src/main/java/edu/gcc/subjecttochange/Main.java
+++ src/main/java/edu/gcc/subjecttochange/Main.java
@@ -16,9 +16,11 @@ public class Main {
 
     public static void main(String[] args) {
         Javalin app = Javalin.create(config -> {
+            // remove cors
             config.bundledPlugins.enableCors(cors -> {
                 cors.addRule(CorsPluginConfig.CorsRule::anyHost);
             });
+            // load production build of React frontend
             config.staticFiles.add("src/main/app/dist", Location.EXTERNAL);
             config.spaRoot.addFile("/", "src/main/app/dist/index.html", Location.EXTERNAL);
         }).start(7070);
--- src/main/java/edu/gcc/subjecttochange/dtos/ScheduleDto.java
+++ src/main/java/edu/gcc/subjecttochange/dtos/ScheduleDto.java
@@ -1,6 +1,5 @@
 package edu.gcc.subjecttochange.dtos;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import edu.gcc.subjecttochange.models.Activity;
