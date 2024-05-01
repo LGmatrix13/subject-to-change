fa1c9416d4ca12cf3e29c9d33896c64d8443d9e4
Liam Grossman <liam.j.grossman@gmail.com>
2024-05-01 14:10:15 -0400

    Update useSearch.ts

--- src/main/app/src/hooks/useSearch.ts
+++ src/main/app/src/hooks/useSearch.ts
@@ -5,15 +5,12 @@ import { useSearchParams } from "react-router-dom";
 import { Course } from "../utils/types";
 
 export default function useSearch() {
-  const [searchParams] = useSearchParams();
   const [user] = useLocalStorage("user", {
     jwt: "",
   });
 
   const { data, isLoading, error } = useSWR<Course[]>(
-    searchParams.toString().length
-      ? `http://localhost:7070/api/search?${searchParams.toString()}`
-      : null,
+    `http://localhost:7070/api/courses`,
     (url: string) => fetcher(url, user.jwt)
   );
 
