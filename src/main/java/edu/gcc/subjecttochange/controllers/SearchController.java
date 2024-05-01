fafe88c95adffbe1259ab29cebcd701aefd420cb
Liam Grossman <liam.j.grossman@gmail.com>
2024-05-01 13:34:05 -0400

    Revert "Make search realtime" (#48)

---
+++ src/main/java/edu/gcc/subjecttochange/controllers/SearchController.java
@@ -0,0 +1,16 @@
+package edu.gcc.subjecttochange.controllers;
+
+import edu.gcc.subjecttochange.dtos.CourseDto;
+import edu.gcc.subjecttochange.utilties.Database;
+import edu.gcc.subjecttochange.models.Search;
+import edu.gcc.subjecttochange.utilties.Response;
+import io.javalin.http.Context;
+
+import java.sql.SQLException;
+
+public class SearchController {
+    public static void getSearch(Context context) throws SQLException {
+        Search search = new Search(context.req());
+        Response.send(200, context, search.run());
+    }
+}
