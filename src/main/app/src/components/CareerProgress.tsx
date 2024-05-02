23f750b602ae2ae063f247c95d578d800e92df9e
Kesterholtem21 <kesterholtem21@gcc.edu>
2024-05-02 15:07:50 -0400

    progress small updates

--- src/main/app/src/components/CareerProgress.tsx
+++ src/main/app/src/components/CareerProgress.tsx
@@ -9,9 +9,9 @@ export default function CareerProgress(props: CareerProgressProps) {
     <section className="p-7 bg-slate-100 rounded-lg space-y-7 custom-shadow">
       <div className="space-y-5">
         <div className="flex flex-col space-y-3">
-          <h2 className="text-2xl font-bold uppercase">Career Progress</h2>
+          <h2 className="text-3xl font-bold uppercase">Career Progress</h2>
           <p>
-            How much progress you will make on major requirements based on the
+            How much progress you will make on other degree requirements based on the
             schedule you have made for the year{" "}
           </p>
         </div>
--- src/main/app/src/pages/ProgressPage.tsx
+++ src/main/app/src/pages/ProgressPage.tsx
@@ -1,5 +1,4 @@
-import CourseTable from "../components/CourseTable";
-import Loading from "../components/Loading";
+
 import CareerProgress from "../components/CareerProgress";
 import { useDelay } from "../hooks/useDelay";
 import useProgress from "../hooks/useProgress";
@@ -10,7 +9,14 @@ export default function ProgressPage() {
   return (
     <CareerProgress>
       <div className="space-y-3">
-        <h3 className="text-x3 font-bold">Progress</h3>
+        <div><h2 className="text-2xl font-bold">REQUIREMENTS</h2></div>
+        <div><h3 className="text-xl font-bold">Humanities Core</h3>
+          <p>Completed 5/5</p>
+        </div>
+        <div><h4 className="text-xl font-bold">Studies in Science, Faith, and Technology</h4></div>
+        <div><h5 className="text-xl font-bold">Writing Requirement</h5></div>
+        <div> <h6 className="text-xl font-bold">Foundations of the Social Sciences</h6></div>
+
       </div>
     </CareerProgress>
   );
